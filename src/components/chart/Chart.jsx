700ed713762496694e95c4f00294aa43d61806de
ismailify <ismailnuralam@gmail.com>
2022-03-19 13:19:01 +0700

    Mulai bangun Chart

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,8 +1,40 @@
 import "./chart.scss";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+  {name: "Januari", Total: 1200},
+  {name: 'Februari', Total: 2100},
+  {name: "March", Total: 800},
+  {name: "April", Total: 1600},
+  {name: "May", Total: 900},
+  {name: "June", Total: 1700}
+];
 
 const Chart = () => {
   return (
-    <div className="chart">Chart</div>
+    <div className="chart">
+       <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
 
