6cfeaa4cb2583e8bfec4bc39a48369ccdd93d51d
ismailify <ismailnuralam@gmail.com>
2022-03-20 11:17:17 +0700

    Buat fitur Table dan juga fitur chart sudah selesai

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,41 +1,55 @@
 import "./chart.scss";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const data = [
-  {name: "Januari", Total: 1200},
-  {name: 'Februari', Total: 2100},
-  {name: "March", Total: 800},
-  {name: "April", Total: 1600},
-  {name: "May", Total: 900},
-  {name: "June", Total: 1700}
+  { name: "January", Total: 1200 },
+  { name: "February", Total: 2100 },
+  { name: "March", Total: 800 },
+  { name: "April", Total: 1600 },
+  { name: "May", Total: 900 },
+  { name: "June", Total: 1700 },
 ];
 
 const Chart = () => {
   return (
     <div className="chart">
-       <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
+      <div className="title">Last 6 Months (Revenue)</div>
+      <ResponsiveContainer width="100%" aspect={3 / 1}>
+        <AreaChart
+          width={730}
+          height={250}
           data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
+          <defs>
+            <linearGradient id="Total" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" stroke="gray" />
+          <YAxis stroke="gray" />
+          <CartesianGrid strokeDasharray="3 3" className="chartGrid" />
           <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </LineChart>
+          <Area
+            type="monotone"
+            dataKey="Total"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#Total)"
+          />
+        </AreaChart>
       </ResponsiveContainer>
     </div>
-  )
-}
+  );
+};
 
-export default Chart
+export default Chart;
---
+++ src/components/table/Table.jsx
@@ -0,0 +1,86 @@
+import "./table.scss";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const TableTransaction = () => {
+    const rows = [
+        {
+            id: 1143155,
+            product: "Hp Omen 15",
+            img: "https://images.idgesg.net/images/article/2017/06/hp_omen_17-laptop_left_rearquarter_jun_2017-100725000-orig.jpg",
+            customer: "Steven Acep",
+            date: "1 Sya'ban",
+            amount: 785,
+            method: "Cash on Delivery",
+            status: "Approved"
+        },
+        {
+            id: 1143155,
+            product: "Playstation 5",
+            img: "https://cdn.pocket-lint.com/r/s/1200x630/assets/images/143354-games-feature-sony-playstation-5-release-date-rumours-and-everything-you-need-to-know-about-ps5-image1-cvz3adase9.jpg",
+            customer: "Cecep Sudrajat",
+            date: "1 Rajab",
+            amount: 785,
+            method: "Online Payments",
+            status: "Pending"
+        },
+        {
+            id: 1143155,
+            product: "Steam Deck",
+            img: "https://knowthisapp.com/wp-content/uploads/2021/08/everything-you-need-to-know-about-steam-deck.jpg",
+            customer: "Uus Dahlia",
+            date: "1 Rabbiul Awal",
+            amount: 785,
+            method: "Online Payments",
+            status: "Pending"
+        },
+        {
+            id: 1143155,
+            product: "Iphone 13 pro max",
+            img: "https://bgr.com/wp-content/uploads/2021/04/iphone-13-design-schematic-3d-leak-1.jpg",
+            customer: "Entin Ningsih",
+            date: "15 Syawal",
+            amount: 785,
+            method: "Cash",
+            status: "Approved"
+        },
+    ]
+
+    return(
+        <TableContainer component={Paper} className="table">
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell className="tableCell">Tracking ID</TableCell>
+            <TableCell className="tableCell">Product</TableCell>
+            <TableCell className="tableCell">Customer</TableCell>
+            <TableCell className="tableCell">Date</TableCell>
+            <TableCell className="tableCell">Amount</TableCell>
+            <TableCell className="tableCell">Payment Method</TableCell>
+            <TableCell className="tableCell">Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell>{row.id}</TableCell>
+              <TableCell className="tableCell">{row.product}</TableCell>
+              <TableCell className="tableCell">{row.customer}</TableCell>
+              <TableCell className="tableCell">{row.date}</TableCell>
+              <TableCell className="tableCell">{row.amount}</TableCell>
+              <TableCell className="tableCell">{row.method}</TableCell>
+              <TableCell className="tableCell">{row.status}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    )
+}
+
+export default TableTransaction;
