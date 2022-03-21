8c6367bfafe1c0206f45d194544d9154f44ddb5b
ismailify <ismailnuralam@gmail.com>
2022-03-21 19:54:17 +0700

    Single screen done and make chart more dynamics

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,6 +1,8 @@
 import "./single.scss";
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
+import Chart from '../../components/chart/Chart';
+import TableTransaction from "../../components/table/Table";
 
 const Single = () => {
   return (
@@ -35,9 +37,14 @@ const Single = () => {
               </div>
             </div>
           </div>
-          <div className="right">Right Screen</div>
+          <div className="right">
+            <Chart aspect={4/1} title={'User Spending (Last 6 months)'} />
+          </div>
         </div>
-        <div className="bottom"></div>
+        <div className="bottom">
+          <h1 className="title">Last Transaction</h1>
+          <TableTransaction />
+        </div>
       </div>
     </div>
   )
