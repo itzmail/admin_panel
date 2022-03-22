412cafea8081cab6481ebd58c5b1c6668f6e387d
ismailify <ismailnuralam@gmail.com>
2022-03-22 13:56:28 +0700

    Menambahkan fitur Tambah gambar

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,9 +1,12 @@
+import {useState} from 'react'
 import "./new.scss";
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
 
 const New = ({inputs, title}) => {
+  const [file, setFile] = useState("");
+
   return (
     <div className="new">
       <Sidebar />
@@ -14,7 +17,7 @@ const New = ({inputs, title}) => {
         </div>
         <div className="bottom">
           <div className="left">
-            <img src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" alt="" />
+            <img src={file ? URL.createObjectURL(file) : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"} alt="" />
           </div>
           <div className="right">
             <form>
@@ -22,7 +25,7 @@ const New = ({inputs, title}) => {
                 <label htmlFor="file">
                   Image: <DriveFolderUploadIcon className="icon" />
                 </label>
-                <input type="file" id="file" style={{display: "none"}} />
+                <input type="file" id="file" onChange={e => setFile(e.target.files[0])} style={{display: "none"}} />
               </div>
               {inputs.map((value, key) => {
                 return(
